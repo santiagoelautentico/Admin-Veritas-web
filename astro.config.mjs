a1106c5f47f7c96ae029967698f9f5213e5ece9d
Santiago <s.urbinazeballos@gmail.com>
2025-12-18 19:57:22 -0300

    server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server', // o 'hybrid' si solo algunas páginas son SSR
+  output: 'server',
   adapter: node({
     mode: 'standalone'
   }),
+  server: {
+    host: true, // Esto hace que escuche en 0.0.0.0
+    port: 10000
+  }
 });
